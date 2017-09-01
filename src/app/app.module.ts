60d22635718f53499887a95c4d2eb47ae49540f4
Michael O'Keefe <m.okeefe@kunzleigh.com>
2017-09-01 08:26:29 -0400

    ng new and added firebase config/modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDkMxqhRj9-Xr9-vqQiFYxfIAqttbk-B3I',
+  authDomain: 'kla-scrum-poker.firebaseapp.com',
+  databaseURL: 'https://kla-scrum-poker.firebaseio.com',
+  projectId: 'kla-scrum-poker',
+  storageBucket: 'kla-scrum-poker.appspot.com',
+  messagingSenderId: '735057512980'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
