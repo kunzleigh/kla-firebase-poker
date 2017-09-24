ebaecbe8a4bb21bb401d18499818b625c702b889
Unknown <c.dahm@kunzleigh.com>
2017-09-23 21:48:37 -0400

    Updating ticketvote

--- src/app/component/ticket-vote/ticket-vote.component.ts
+++ src/app/component/ticket-vote/ticket-vote.component.ts
@@ -1,16 +1,60 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {TicketService} from '../../service/ticket.service';
 import {NavService} from "../../service/nav.service";
+import {FIBONACCI, Fibonacci} from "../../class/fibonacci";
+import {Ticket} from "../../class/ticket";
+import {ISubscription} from "rxjs/Subscription";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-ticket-vote',
   templateUrl: './ticket-vote.component.html',
   styleUrls: ['./ticket-vote.component.css']
 })
-export class TicketVoteComponent {
+export class TicketVoteComponent implements OnInit, OnDestroy {
 
-  constructor(public ticketService: TicketService, public navService: NavService) {
+  readonly FIBONOCCI = FIBONACCI;
 
+  current: Ticket;
+  selected: Fibonacci;
+
+  private _routeSubscription: ISubscription;
+
+  constructor(public ticketService: TicketService, public navService: NavService, private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    this._routeSubscription = this.route.params.subscribe(params => {
+      const ticketId = params['$ticketId'];
+      this.ticketService.ticketList$.subscribe((tickets: Ticket[]) => {
+        tickets.forEach((ticket) => {
+          if (ticket.$key === ticketId) {
+            this.current = ticket;
+          }
+        });
+      });
+    });
+  }
+
+  ngOnDestroy() {
+    this._routeSubscription.unsubscribe();
+  }
+
+  selectVote(fibonocci: Fibonacci) {
+    this.selected = fibonocci;
+  }
+
+  saveVote() {
+
+  }
+
+  clearVote() {
+    this.selected = null;
+  }
+
+  goToTicketList() {
+    this.navService.navigate('/tickets');
   }
 
 }
