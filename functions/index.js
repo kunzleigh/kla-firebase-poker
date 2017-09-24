1be2501ed26b12f9c95b931ebfda0f914d6d943d
Jason Kusnier <jkusnier@gmail.com>
2017-09-24 16:27:26 -0400

    added example http function

--- functions/index.js
+++ functions/index.js
@@ -13,3 +13,21 @@ exports.setupNewUser = functions.auth.user().onCreate(event => {
     imageUrl: "https://firebasestorage.googleapis.com/v0/b/kla-scrum-poker.appspot.com/o/user-profile-images%2Fno-image.png?alt=media&token=663c6ead-4d9d-4e78-8233-0c4e8da22636"
   });
 });
+
+exports.ticketStats = functions.https.onRequest((req, res) => {
+  // Result object that will be returned
+  const result = {
+    totalTickets: 0,
+  };
+
+  // This will not scale well. A database trigger would be better.
+  admin.database().ref('/tickets').once("value").then(function(snapshot) {
+    const collection = snapshot.val();
+
+    // Count of tickets
+    result.totalTickets = Object.keys(collection).length;
+
+    res.send(result);
+  });
+
+});
