bf2f246dcbb4a503c517e235ef592e0a7d3612e8
Michael O'Keefe <m.okeefe@kunzleigh.com>
2017-09-25 09:59:33 -0400

    added promise delay for ticket loading

--- src/app/resolve/ticket-vote.resolve.ts
+++ src/app/resolve/ticket-vote.resolve.ts
@@ -3,6 +3,7 @@ import {ActivatedRouteSnapshot, Resolve, Router} from '@angular/router';
 import {LoaderService} from '../service/loader.service';
 import {TicketService} from "../service/ticket.service";
 import {Ticket} from "../class/ticket";
+import { loadingDelay } from '../../settings/loading';
 
 @Injectable()
 export class TicketVoteResolve implements Resolve<Ticket> {
@@ -13,13 +14,19 @@ export class TicketVoteResolve implements Resolve<Ticket> {
     this.loaderService.showLoader();
     const ticketId = route.paramMap.get('$ticketId');
     return this.ticketService.getCurrentTicket(ticketId).then((ticket) => {
-      this.loaderService.hideLoader();
-      if (ticket) {
-        return ticket;
-      } else {
-        this.router.navigate(['/home']);
-      }
+      return this.promiseDelay().then(() => {
+        this.loaderService.hideLoader();
+        if (ticket) {
+          return ticket;
+        } else {
+          this.router.navigate(['/home']);
+        }
+      });
     });
+  }
+
+  private promiseDelay(): Promise<any> {
+    return new Promise(resolve => setTimeout(resolve, loadingDelay));
   }
 
 }
