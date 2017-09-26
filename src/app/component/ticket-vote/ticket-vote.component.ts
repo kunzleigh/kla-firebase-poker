9e6ff00376f6a590337d9d53f52df6d5aa1c5fae
Unknown <c.dahm@kunzleigh.com>
2017-09-25 21:00:24 -0400

    Adjusting components to use new functions/observables

--- src/app/component/ticket-vote/ticket-vote.component.ts
+++ src/app/component/ticket-vote/ticket-vote.component.ts
@@ -1,11 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {TicketService} from '../../service/ticket.service';
 import {NavService} from "../../service/nav.service";
-import {FIBONACCI, Fibonacci} from "../../class/fibonacci";
-import {Ticket} from "../../class/ticket";
-import {ActivatedRoute} from "@angular/router";
+import {FIBONACCI} from "../../class/fibonacci";
 import {AuthService} from "../../service/auth.service";
-import {Vote} from "../../class/vote";
 import {VoteService} from "../../service/vote.service";
 
 @Component({
@@ -13,45 +10,11 @@ import {VoteService} from "../../service/vote.service";
   templateUrl: './ticket-vote.component.html',
   styleUrls: ['./ticket-vote.component.css']
 })
-export class TicketVoteComponent implements OnInit {
+export class TicketVoteComponent {
 
   readonly FIBONOCCI = FIBONACCI;
 
-  current: Ticket;
-  votes: Vote[] = [];
-
-  constructor(public ticketService: TicketService, public navService: NavService, private route: ActivatedRoute, public authService: AuthService, private voteService: VoteService) {
-
-  }
-
-  ngOnInit() {
-    this.current = this.route.snapshot.data['ticketVote'];
-    if (this.current && this.current.votes) {
-      let voteKeys = Object.keys(this.current.votes) || [];
-      voteKeys.forEach((it) => {
-        this.votes.push(this.current.votes[it]);
-      });
-    }
-  }
-
-  getVotes() {
-    return this.votes || [];
-  }
-
-  selectVote(fibonocci: Fibonacci, hasVote: boolean) {
-    debugger;
-    if (hasVote) {
-      this.voteService.setVote(fibonocci, this.current.$key);
-    } else {
-      this.voteService.addVote(fibonocci, this.current.$key)
-    }
-  }
-
-  saveVote() {
-
-  }
-
-  clearVote() {
+  constructor(public ticketService: TicketService, public navService: NavService, public authService: AuthService, public voteService: VoteService) {
 
   }
 
