947ba327c89079b6e8d8df542e8f46febbfe2113
Unknown <c.dahm@kunzleigh.com>
2017-09-25 20:59:54 -0400

    Adjusting resolver to not provide route data

--- src/app/resolve/ticket-vote.resolve.ts
+++ src/app/resolve/ticket-vote.resolve.ts
@@ -1,26 +1,21 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, Resolve, Router} from '@angular/router';
+import {ActivatedRouteSnapshot, Resolve} from '@angular/router';
 import {LoaderService} from '../service/loader.service';
 import {TicketService} from "../service/ticket.service";
-import {Ticket} from "../class/ticket";
 import { loadingDelay } from '../../settings/loading';
 
 @Injectable()
-export class TicketVoteResolve implements Resolve<Ticket> {
+export class TicketVoteResolve implements Resolve<boolean> {
 
-  constructor(private ticketService: TicketService, private loaderService: LoaderService, private router: Router) { }
+  constructor(private ticketService: TicketService, private loaderService: LoaderService) { }
 
-  resolve(route: ActivatedRouteSnapshot): Promise<Ticket> {
+  resolve(route: ActivatedRouteSnapshot): Promise<boolean> {
     this.loaderService.showLoader();
     const ticketId = route.paramMap.get('$ticketId');
-    return this.ticketService.getCurrentTicket(ticketId).then((ticket) => {
+    return this.ticketService.getCurrentTicket(ticketId).then(() => {
       return this.promiseDelay().then(() => {
         this.loaderService.hideLoader();
-        if (ticket) {
-          return ticket;
-        } else {
-          this.router.navigate(['/home']);
-        }
+        return true;
       });
     });
   }
